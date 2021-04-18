a30b3fb7345b6f11f0b0db6ddee576b7d4e7be21
Kalyani1239 <kalyani.rosy@gmail.com>
2021-04-18 16:37:59 +0530

    Initial commit

---
+++ Test/Demo.py
@@ -0,0 +1,7 @@
+
+
+from selenium import webdriver
+
+driver = webdriver.Chrome("C:\\Users\\Kalyani\\PycharmProjects\\FirstProject\\Drivers\\chromedriver.exe")
+
+driver.get("https://www.udemy.com/")
